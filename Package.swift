d90d0f1395b17d14ea94f3460b94c56502ca9a22
Artyom Vasilyev <artem.9999@mail.ru>
2022-05-12 15:20:26 +0300

    Fix ASVideoNode

--- Package.swift
+++ Package.swift
@@ -17,15 +17,19 @@ let headersSearchPath: [CSetting] = [.headerSearchPath("."),
                                      .headerSearchPath("TextKit"),
                                      .headerSearchPath("tvOS"),]
 
-let sharedDefines: [CSetting] = [
-                                // Disable "old" textnode by default for SPM
-                                .define("AS_ENABLE_TEXTNODE", to: "0"),
+let sharedDefines: [CSetting] =
+[
+    // Disable "old" textnode by default for SPM
+    .define("AS_ENABLE_TEXTNODE", to: "0"),
     
-                                // PINRemoteImage always available for Texture
-                                .define("AS_PIN_REMOTE_IMAGE", to: "1"),
-                                
-                                // always disabled
-                                .define("IG_LIST_COLLECTION_VIEW", to: "0"),]
+    // PINRemoteImage always available for Texture
+    .define("AS_PIN_REMOTE_IMAGE", to: "1"),
+    
+    // always disabled
+    .define("IG_LIST_COLLECTION_VIEW", to: "0"),
+    
+    .define("AS_USE_VIDEO", to: "1"),
+]
 
 func IGListKit(enabled: Bool) -> [CSetting] {
     let state: String = enabled ? "1" : "0"
